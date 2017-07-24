ca25de0017070e75a777d22c55f3007f0dce1ede
idanz19-meet <idanz19@meet.mit.edu>
2017-07-24 11:27:35 +0300

    Stopping here

---
+++ labtwo.py
@@ -0,0 +1,35 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> my_name 'student'
+SyntaxError: invalid syntax
+>>> my_name = 'student'
+>>> print("Hi, " + myName')
+      
+SyntaxError: EOL while scanning string literal
+>>> my_name = 'student'
+>>> print("Hi, " + my_name)
+Hi, student
+>>> my_age = 15
+>>> print('I am ' + my_age
+
+      g)
+SyntaxError: invalid syntax
+>>> score = 1
+>>> total = score + (count * 2)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    total = score + (count * 2)
+NameError: name 'count' is not defined
+>>> score = 1
+>>> total = score + (count*2)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    total = score + (count*2)
+NameError: name 'count' is not defined
+>>> score = 1
+>>> count = 5
+>>> total = score + (count * 2)
+>>> print(total)
+11
+>>> 
